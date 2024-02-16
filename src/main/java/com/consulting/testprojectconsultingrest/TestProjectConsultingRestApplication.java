c01dc75df69d3c43f93e49446dee5982624c5708
Sonethe <sniperant@yandex.ru>
2024-02-16 17:19:45 +0300

    Initial commit

---
+++ src/main/java/com/consulting/testprojectconsultingrest/TestProjectConsultingRestApplication.java
@@ -0,0 +1,13 @@
+package com.consulting.testprojectconsultingrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestProjectConsultingRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestProjectConsultingRestApplication.class, args);
+    }
+
+}
